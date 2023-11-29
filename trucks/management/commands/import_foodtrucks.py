4ef5624fc3253607b13bc7421d2cd7bc73ee0b89
guilhermevbeira <guilherme.vieira.beira@gmail.com>
2023-11-29 15:54:56 -0300

    Add truck models

---
+++ trucks/management/commands/import_foodtrucks.py
@@ -0,0 +1,25 @@
+from django.contrib.gis.geos import Point
+from django.core.management.base import BaseCommand
+
+import csv
+from trucks.models import FoodTruck
+
+
+class Command(BaseCommand):
+    help = "Import food trucks from a CSV file"
+
+    def add_arguments(self, parser):
+        parser.add_argument("csv_file", type=str, help="The CSV file to import")
+
+    def handle(self, *args, **options):
+        file_path = options["csv_file"]
+        with open(file_path, newline="", encoding="utf-8") as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                FoodTruck.objects.create(
+                    name=row["Applicant"],
+                    facility_type=row["FacilityType"],
+                    location=Point(float(row["Longitude"]), float(row["Latitude"])),
+                    address=row["Address"],
+                )
+        self.stdout.write(self.style.SUCCESS("Successfully imported food trucks"))
--- trucks/models.py
+++ trucks/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+
+class FoodTruck(models.Model):
+    name = models.CharField(max_length=255)
+    facility_type = models.CharField(max_length=100)
+    location = models.PointField()
+    address = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.applicant
