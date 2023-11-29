145d5e354d54046591dcd8269a7ae1d117e60134
guilhermevbeira <guilherme.vieira.beira@gmail.com>
2023-11-29 18:02:26 -0300

    Add initial filter logic

---
+++ trucks/filters.py
@@ -0,0 +1,27 @@
+from django.contrib.gis.db.models.functions import Distance
+from django.contrib.gis.geos import Point
+from django_filters import rest_framework as filters
+from .models import FoodTruck
+
+
+class FoodTruckDistanceFilter(filters.FilterSet):
+    distance = filters.NumberFilter(method="filter_distance")
+    latitude = filters.NumberFilter(method="filter_distance")
+    longitude = filters.NumberFilter(method="filter_distance")
+
+    class Meta:
+        model = FoodTruck
+        fields = ["distance", "latitude", "longitude"]
+
+    def filter_distance(self, queryset, name, value):
+        if name == "distance" and "latitude" in self.data and "longitude" in self.data:
+            print(f"filtering by distance {value}")
+            latitude = self.data["latitude"]
+            longitude = self.data["longitude"]
+            user_location = Point(float(longitude), float(latitude), srid=4326)
+            return (
+                queryset.annotate(distance=Distance("location", user_location))
+                .filter(location__distance_lte=(user_location, value))
+                .order_by("distance")
+            )
+        return queryset
--- trucks/views.py
+++ trucks/views.py
@@ -1,8 +1,10 @@
 from rest_framework import viewsets
 from .models import FoodTruck
 from .serializers import FoodTruckSerializer
+from .filters import FoodTruckDistanceFilter
 
 
 class FoodTruckListView(viewsets.ModelViewSet):
     queryset = FoodTruck.objects.all().order_by("name")
     serializer_class = FoodTruckSerializer
+    filterset_class = FoodTruckDistanceFilter
